fb00b5d4025460b04adec6df4da82a16f50aeeac
Stefen Sharkey <stefenatefun@gmail.com>
2016-11-20 14:06:41 -0500

    Move entity code into seperate namespace.

--- RoguelikeGame/Entity/Entity.cs
+++ RoguelikeGame/Entity/Entity.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace RoguelikeGame
+namespace Entity
 {
     class Entity : Sprite
     {
