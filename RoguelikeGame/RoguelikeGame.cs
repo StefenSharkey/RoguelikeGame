59b60f8a23d22e1a659f5b242f1ff931d85ec7fd
Stefen Sharkey <stefenatefun@gmail.com>
2016-11-26 02:08:15 -0500

    Add start menu.

--- RoguelikeGame/RoguelikeGame.cs
+++ RoguelikeGame/RoguelikeGame.cs
@@ -16,7 +16,8 @@ namespace RoguelikeGameNamespace
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        private SpriteFont font;
+        private SpriteFont titleFont;
+        private SpriteFont textFont;
 
         private Player player;
         public static List<Enemy> enemies = new List<Enemy>();
@@ -30,6 +31,18 @@ namespace RoguelikeGameNamespace
             Playing,
             Paused
         }
+        private GameState currentGameState = GameState.StartMenu;
+
+        private Vector2 titlePos;
+        private Vector2 startButtonPos;
+        private Vector2 exitButtonPos;
+
+        private string titleText = "Roguelike Game";
+        private string startButtonText = "Start";
+        private string exitButtonText = "Exit";
+
+        private Rectangle startButtonRectangle;
+        private Rectangle exitButtonRectangle;
 
         public RoguelikeGame()
         {
@@ -46,6 +59,8 @@ namespace RoguelikeGameNamespace
         protected override void Initialize()
         {
             IsMouseVisible = true;
+
+            currentGameState = GameState.StartMenu;
 
             player = new Player("Player", 0, 0);
             enemies.Add(new Enemy("Enemy", 120, 80));
@@ -68,7 +83,15 @@ namespace RoguelikeGameNamespace
                 enemy.LoadContent(this.Content);
             }
 
-            font = Content.Load<SpriteFont>("Health");
+            titleFont = Content.Load<SpriteFont>("Title");
+            textFont = Content.Load<SpriteFont>("Text");
+
+            titlePos = new Vector2((graphics.GraphicsDevice.Viewport.Width - titleFont.MeasureString(titleText).X) / 2, 0);
+            startButtonPos = new Vector2((graphics.GraphicsDevice.Viewport.Width - textFont.MeasureString(startButtonText).X) / 2, titleFont.MeasureString(titleText).Y);
+            exitButtonPos = new Vector2((graphics.GraphicsDevice.Viewport.Width - textFont.MeasureString(exitButtonText).X) / 2, startButtonPos.Y + textFont.MeasureString(startButtonText).Y);
+
+            startButtonRectangle = new Rectangle((int)startButtonPos.X, (int)startButtonPos.Y, (int)textFont.MeasureString(startButtonText).X, (int)textFont.MeasureString(startButtonText).Y);
+            exitButtonRectangle = new Rectangle((int)exitButtonPos.X, (int)exitButtonPos.Y, (int)textFont.MeasureString(exitButtonText).X, (int)textFont.MeasureString(exitButtonText).Y);
         }
 
         /// <summary>
@@ -92,11 +115,33 @@ namespace RoguelikeGameNamespace
                 Exit();
             }
 
-            player.Update(gameTime);
-
-            foreach (Enemy enemy in enemies)
+            switch (currentGameState)
             {
-                enemy.Update(gameTime);
+                case GameState.StartMenu:
+                    MouseState mouseState = Mouse.GetState();
+                    Point mousePos = new Point(mouseState.X, mouseState.Y);
+
+                    if (mouseState.LeftButton == ButtonState.Pressed)
+                    {
+                        if (startButtonRectangle.Contains(mousePos))
+                        {
+                            currentGameState = GameState.Playing;
+                        }
+                        else if (exitButtonRectangle.Contains(mousePos))
+                        {
+                            Exit();
+                        }
+                    }
+
+                    break;
+                case GameState.Playing:
+                    player.Update(gameTime);
+
+                    foreach (Enemy enemy in enemies)
+                    {
+                        enemy.Update(gameTime);
+                    }
+                    break;
             }
 
             base.Update(gameTime);
@@ -108,19 +153,29 @@ namespace RoguelikeGameNamespace
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            string healthText = "Health: " + health;
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             spriteBatch.Begin();
 
-            player.Draw(this.spriteBatch);
-
-            foreach (Enemy enemy in enemies)
+            switch (currentGameState)
             {
-                enemy.Draw(this.spriteBatch);
-            }
+                case GameState.StartMenu:
+                    spriteBatch.DrawString(titleFont, titleText, titlePos, Color.Black);
+                    spriteBatch.DrawString(textFont, startButtonText, startButtonPos, Color.Black);
+                    spriteBatch.DrawString(textFont, exitButtonText, exitButtonPos, Color.Black);
+                    break;
+                case GameState.Playing:
+                    string healthText = "Health: " + health;
+                    player.Draw(this.spriteBatch);
 
-            spriteBatch.DrawString(font, healthText, new Vector2(0, graphics.GraphicsDevice.PresentationParameters.Bounds.Height - font.MeasureString(healthText).Y), Color.Black);
+                    foreach (Enemy enemy in enemies)
+                    {
+                        enemy.Draw(this.spriteBatch);
+                    }
+
+                    spriteBatch.DrawString(textFont, healthText, new Vector2(0, graphics.GraphicsDevice.Viewport.Height - textFont.MeasureString(healthText).Y), Color.Black);
+                    break;
+            }
 
             spriteBatch.End();
 
